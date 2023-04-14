2f63ddf83eafefaf949b258a84f18c3eb0df55c9
KrYounger <35814586+xt0fer@users.noreply.github.com>
2023-04-14 14:39:47 -0400

    checkpoint

--- src/test/java/rocks/zipcode/jvi/BufferTest.java
+++ src/test/java/rocks/zipcode/jvi/BufferTest.java
@@ -4,14 +4,9 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Mock;
-import org.mockito.junit.MockitoJUnitRunner;
 
-@RunWith(MockitoJUnitRunner.class)
 public class BufferTest {
-	@Mock
-	private int maxLines;
+	private int maxLines = 24;
 
 	private Buffer buffer;
 
